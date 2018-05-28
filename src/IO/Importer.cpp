c9e2dbd21184d7f5efef3fcde645cd3c0de68d03
Tadeáš Valenta <tadashicz@gmail.com>
2018-05-28 23:27:47 +0200

    add Importer class

--- src/IO/Importer.cpp
+++ src/IO/Importer.cpp
@@ -2,4 +2,60 @@
 // Created by Tadeáš Valenta on 28.5.18.
 //
 
+#include <fstream>
 #include "Importer.hpp"
+
+#define CHECK_READ_FILE if ( inputFile . fail()) {cout << "Read from file failed" << endl; return false;}
+#define EMPTY_VECTOR vector<Quiz>()
+#define CHECK_WRITE_FILE if ( outputFile . fail()) {cout <<"Write to file failed" << endl; return false;}
+
+using namespace std;
+
+bool Importer::loadFromFile(string filename, vector<Quiz> quizzes) {
+	std::string input;
+	ifstream inputFile( filename );
+	CHECK_READ_FILE
+	getline( inputFile, input );
+	int quizNumber = atoi( input.c_str() );
+
+	for ( int i = 0 ; i < quizNumber ; ++i ) {
+		Quiz quiz = loadQuiz( inputFile );
+		if ( quiz.mName.empty() ) {
+			return false;
+		}
+		quizzes.push_back( quiz );
+	}
+	return true;
+}
+
+Quiz Importer::loadQuiz(ifstream & inputString) {
+	Quiz quiz;
+	string input;
+	getline(inputString, input);
+	if (input.c_str() != "Quiz"){
+		quiz.mName = "";
+		return quiz;
+	}
+
+	getline(inputString, quiz.mName);
+	getline(inputString, quiz.mAuthor);
+
+	getline(inputString, input);
+	int pageCount = atoi(input.c_str());
+
+	loadTree(pageCount);
+
+	for ( int i = 0 ; i <  pageCount; ++i ) {
+		loadPage()
+	}
+
+	return quiz;
+}
+
+Page Importer::loadPage(ifstream & inputString) {
+	return Page();
+}
+
+shared_ptr<Question> Importer::loadQuestion(ifstream & inputString) {
+	return shared_ptr<Question>();
+}
--- src/IO/Importer.hpp
+++ src/IO/Importer.hpp
@@ -5,8 +5,31 @@
 #ifndef SEMESTRAL_PROJECT_IMPORTER_HPP
 #define SEMESTRAL_PROJECT_IMPORTER_HPP
 
+#include <iostream>
+#include "../Game/Quiz.hpp"
+#include "../Game/Question/Question.hpp"
 
-class Importer {
+/**
+ * Namespace with functions to import
+ */
+namespace Importer {
+	/**
+	 * Loads quizzes from given file
+	 * @param filename	filename to read from
+	 * @param quizzes	vector of quiz files to save the quizzes loaded from the file to
+	 * @return			true if reading the whole file was successful, false otherwise
+	 */
+bool loadFromFile(std::string filename, std::vector<Quiz> quizzes);
+
+/**
+ * Loads one quiz from filestream
+ * @param inputString filestream to read from
+ * @return quiz
+ */
+Quiz loadQuiz(std::ifstream & inputString);
+Page loadPage(std::ifstream & inputString);
+std::shared_ptr<Question> loadQuestion(std::ifstream & inputString);
+
 
 };
 
